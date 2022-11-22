82af5c57b1ca56a981555b19a25c3e315e2efcbd
g_garsan@hotmail.com <g_garsan@hotmail.com>
2022-11-22 13:24:12 +0100

    attributes added to posts page

--- frontend/src/app/home/home-routing.module.ts
+++ frontend/src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostsPage } from '../posts/posts.page';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
+        {
+          path: '',
+          component: HomePage,
+        },
   {
-    path: '',
-    component: HomePage,
+    path: 'posts',
+    component: PostsPage,
   }
 ];
 
--- frontend/src/app/posts/posts.page.ts
+++ frontend/src/app/posts/posts.page.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage implements OnInit {
-
+  posts = [
+        {
+                id: '1',
+                title: 'Black Train',
+                comment:'the nice train',
+                imageURL: 'https://pngimg.com/uploads/trian/small/trian_PNG16647.png'
+        },
+        {
+                id: '2',
+                title: 'Locomotive',
+                comment:'Locomotive with smoke',
+                imageURL: 'https://avatarfiles.alphacoders.com/582/58289.jpg'
+        },
+        {
+                id: '3',
+                title: 'Grey locomotive',
+                comment:'Spectacular locomotive',
+                imageURL: 'https://mcdn.wallpapersafari.com/318/26/26/eWaSz1.jpg'
+        },
+        {
+                id: '4',
+                title: 'Modern train',
+                comment:'Nice train picture',
+                imageURL: 'https://img-aws.ehowcdn.com/400x400/ds-img.studiod.com/Acela_old_saybrook_ct_summer2_1.jpg'
+        }
+ ]
   constructor() { }
 
   ngOnInit() {
